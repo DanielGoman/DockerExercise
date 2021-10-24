aa228b883f81b30565760da261762f561d56bfe5
Daniel Goman <gomanGGGoman@gmail.com>
2021-10-24 22:56:56 +0300

    Fixed the run.cmd commands (docker build), they were incorrect

--- run.cmd
+++ run.cmd
@@ -1,4 +1,4 @@
-docker build --tag=left_image left/left_image
-docker build --tag=right_image right/right_image
+docker build --tag=left_image --file=left_image .
+docker build --tag=right_image --file=right_image .
 
 docker-compose up
